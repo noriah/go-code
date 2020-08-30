d81bc63cb45c0aadbb39ea98e21888a7110cfb0d
noriah <vix@noriah.dev>
2020-08-30 04:20:38 -0700

    buzz

--- fizzbuzz/main.go
+++ fizzbuzz/main.go
@@ -1,5 +1,6 @@
 package main
 
 func main() {
-	fizzBuzzShift()
+	fizzBuzzTable()
+	// fizzBuzzShift()
 }
--- fizzbuzz/table.go
+++ fizzbuzz/table.go
@@ -1,32 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func fizzBuzzTable() {
 
-	s := ""
-	j := 0
-	for i, x, y, z, q := 1, 1, 1, 1, 1; i <= 100; i++ {
-		// x, y, z = i%15, i%3, i%5
-		q = (q + i)
+	// define our variables
+	var num, div3, div5, div15, flag int
+	var star rune
 
-		x = (i % 15)
-		y = (i % 3)
-		z = (i % 5)
+	for num = 0; num <= 100; num++ {
 
-		if y == 0 || z == 0 {
-			s = "*"
-		} else {
-			s = " "
-		}
+		// num MOD 3 will return the remainder after dividing
+		// by 3 as much as possible.
+		// if no remainder, num is divisble by 3
+		div3 = num % 3
 
-		j = (1 >> y) | (2 >> z)
+		// num MOD 5 will return the remainder after dividing
+		// by 5 as much as possible.
+		// if no remainder, num is divisble by 5
+		div5 = num % 5
 
-		fmt.Printf("%s %3d | %08b ||| %2d | %04b ||| %2d | %04b ||| %2d | %08b\n", s, i, i, y, y, z, z, x, x)
+		// num MOD 15 will return the remainder after dividing
+		// by 15 as much as possible.
+		// if no remainder, num is divisble by 15
+		div15 = num % 15
 
-		if false {
-			fmt.Println(i, "|", x, y, z, "|", y+z, (x % 3), (x % 5), "|", (i%15)%3, "|", fmt.Sprintf("%04b", (i%15)), "|", i, j)
-		}
-		q = q % 15
+		// If div3 or div5 are not 0, we right shift their value
+		// 0x01 by any amount, making it 0. OR the two shifts and
+		// we now have a flag representing divisble by 3 or 5.
+		//
+		// Alternatively, add the shifts to get number of conditions met
+		flag = (1 >> div3) | (1 >> div5)
+
+		// space is 0x20 (32)
+		// star is 0x2a
+		// difference 0x0a
+		// multiply the flag by the difference and add that to space
+		// This will return space when there is no divide, and star otherwise
+		star = rune(0x20 + flag*0x0a)
+
+		// Print our numbers
+		fmt.Printf("%[1]c %3[2]d | %08[2]b ||| %2[3]d | %04[3]b ||| %2[4]d | %04[4]b ||| %2[5]d | %04[5]b\n", star, num, div3, div5, div15)
 	}
 }
