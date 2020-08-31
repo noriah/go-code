764e047ae6eb8f0cea6d1c5782d3df101a2485db
noriah <vix@noriah.dev>
2020-08-30 20:21:11 -0700

    Uhhhhh

--- fizzbuzz/shift.go
+++ fizzbuzz/shift.go
@@ -42,3 +42,43 @@ func Shift() {
 		fmt.Println(fizzBuzz[start:end])
 	}
 }
+
+// Shift2 does FizzBuzz by shifting bits but its different somehow?
+func Shift2() {
+
+	// declare our vars
+	var num, div3, div5, start, end int
+
+	// Our string of FizzBuzz
+	fizzBuzz := "FizzBuzz"
+
+	// loop through the numbers 1 to 100
+	// assign some variables used every loop
+	for num = 1; num <= 100; num++ {
+		// if num is divisble by 3, num mod 3 will return 0
+		div3 = num % 3
+
+		// if num is divisble by 5, num mod 5 will return 0
+		div5 = num % 5
+
+		// Calculate the start
+		start = (0x18 >> div3) & 0x04
+
+		// Calculate the end
+		end = 0x04 << (0x01 >> div5)
+
+		// The above calculations clamp the start to 0 or 4, and
+		// clamp the end to 4 or 8. when both are 4, the slice will be empty
+		if start == end {
+
+			// If we don't have a flag, just print the number
+			fmt.Println(num)
+
+			// go to the beginning of this loop
+			continue
+		}
+
+		// Print it out
+		fmt.Println(fizzBuzz[start:end])
+	}
+}
